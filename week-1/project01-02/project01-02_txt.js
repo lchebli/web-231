5eb436ec1d73bbffd5eb01051ccd212e61d08b41
Leslie Khattarchebli <lkhattarchebli@my365.bellevue.edu>
2025-03-23 12:14:39 -0700

    project01-02

--- week-1/project01-02/project01-02_txt.js
+++ week-1/project01-02/project01-02_txt.js
@@ -2,10 +2,19 @@
     Chapter 1
     Hands-On Project 1-2
 
-    Author: 
-    Date:   
+    Author: Leslie Khattarchebli
+    Date:3/23/25
 
     Filename: project01-02.js
 */
 
 //define variables for service name and service speed
+
+let service1Name = "Basic";
+let service2Name = "Express";
+let service3Name = "Extreme";
+let service4Name = "Ultimate";
+let service1Speed = "50 Mbps";
+let service2Speed = "100 Mbps";
+let service3Speed = "500 Mbps";
+let service4Speed = "1 Gig";
